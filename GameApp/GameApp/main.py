b856fa762bb02d956fc1024fe06a3415199e066d
Goju-Ryu <mads1606@gmail.com>
2020-03-15 01:45:29 +0100

    samlede det hele til en fil

--- GameApp/GameApp/main.py
+++ GameApp/GameApp/main.py
@@ -6,10 +6,60 @@
 #                                        \__/                                                                #
 #      Great help and example snippets from: https://www.101computing.net/getting-started-with-pygame/       #
 ##############################################################################################################
-from GameApp import Gallow
+#from GameApp import Gallow
 import sys
 import zeep
 import pygame
+
+
+class Gallow(pygame.sprite.Sprite):
+    # This class represents a car. It derives from the "Sprite" class in Pygame.
+    __wrong__ = 0
+
+    def __init__(self, x, y):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Instead we could load a proper pciture of a car...
+        self.image = pygame.image.load("pkg_resources/images/wrong0.png")
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.__wrong__ = 0
+
+    def init_with_wrongs(self, x, y, wrongs):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        self.__init__(x, y)
+
+        if not isinstance(wrongs, int):
+            raise TypeError("bar must be set to an integer")
+        if not wrongs >= 0 & wrongs < 7:
+            return
+
+        # Instead we could load a proper pciture of a car...
+        self.image = pygame.image.load("pkg_resources/images/wrong" + wrongs.__str__() + ".png")
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+
+        self.__wrong__ = wrongs
+
+    def increment(self):
+        self.__wrong__ += 1
+        try:
+            # Instead we could load a proper pciture of a car...
+            self.image = pygame.image.load("pkg_resources/images/wrong" + self.__wrong__.__str__() + ".png")
+
+        except:
+            print("couldnt find: " + "pkg_resources/images/wrong" + self.__wrong__.__str__() + ".png")
+
+
+
+
 pygame.init()
 
 username, password = sys.argv[1], sys.argv[2]
@@ -43,7 +93,7 @@ middle = offset * 3
 divWidth = round(middle / 4)
 
 
-gallow = Gallow.Gallow(screen.get_rect().midtop[0] - 85, 25)
+gallow = Gallow(screen.get_rect().midtop[0] - 85, 25)
 spriteList = pygame.sprite.Group()
 spriteList.add(gallow)
 
